73702198400ecf18ad91f03e3d87c42c1baa3979
cpocs <colephillips1@gmail.com>
2017-04-19 21:17:03 -0700

    Fixed scrolling and added +s at the end of "times" when the button gets tapped.

--- app/src/main/java/com/counter/cole/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/counter/cole/buttonclickcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.counter.cole.buttonclickcounter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,11 +23,16 @@ public class MainActivity extends AppCompatActivity {
         userInput = (EditText) findViewById(R.id.editText); // R = resources
         button = (Button) findViewById(R.id.button);
         TextView = (TextView) findViewById(R.id.textView);
+        TextView.setText("");
+        TextView.setMovementMethod(new ScrollingMovementMethod());
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
-            public void onClick(View v) {  
+            public void onClick(View v) {
                 numTimesClicked = numTimesClicked + 1;
                 String result = "\nThe button got tapped " + numTimesClicked + " time";
+                if(numTimesClicked != 1) {
+                    result += "s";    // this is the same as result = result + "s";
+                }
                 TextView.append(result);
 
             }
