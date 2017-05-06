348375ae6c7cd2f7d28b2304c1693ab9404cc5a8
cpocs <colephillips1@gmail.com>
2017-05-06 16:54:57 -0700

    Added logging and other methods

--- app/src/main/java/com/counter/cole/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/counter/cole/buttonclickcounter/MainActivity.java
@@ -3,40 +3,82 @@ package com.counter.cole.buttonclickcounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static com.counter.cole.buttonclickcounter.R.id.editText;
+import static com.counter.cole.buttonclickcounter.R.id.textView;
 
+public class MainActivity extends AppCompatActivity {
     private EditText userInput;
-    private Button button;
     private TextView TextView;
-    private int numTimesClicked = 0;
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userInput = (EditText) findViewById(R.id.editText); // R = resources
-        button = (Button) findViewById(R.id.button);
-        TextView = (TextView) findViewById(R.id.textView);
+        userInput = (EditText) findViewById(editText); // R = resources
+        userInput.setText(""); // Set text to none on startup
+        Button button = (Button) findViewById(R.id.button);
+        TextView = (TextView) findViewById(textView);
+        TextView.clearComposingText();
         TextView.setText("");
         TextView.setMovementMethod(new ScrollingMovementMethod());
         View.OnClickListener ourOnClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numTimesClicked = numTimesClicked + 1;
-                String result = "\nThe button got tapped " + numTimesClicked + " time";
-                if(numTimesClicked != 1) {
-                    result += "s";    // this is the same as result = result + "s";
-                }
+                String result = userInput.getText().toString();
+                result = result + '\n';
                 TextView.append(result);
-
+                userInput.setText(""); // clear text on rotation
             }
         };
         button.setOnClickListener(ourOnClickListener);
+        Log.d(TAG, "onCreate: out");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: in");
+        super.onStart();
+        Log.d(TAG, "onStart: out");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
